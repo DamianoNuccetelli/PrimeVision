dacd98474bd08cbff9c0991cfa41025c6d1220c5
DamianoNuccetelli <damianonuccetelli1997@gmail.com>
2024-03-07 17:03:33 +0100

    Implementata Identity

---
+++ WebAppPrimeVision/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppPrimeVision.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
