06df6e4fef488fd88f77a725965b567d7f00e1e7
GabrieleA <gabrieleaggio26@gmail.com>
2024-03-29 00:41:24 +0100

    Backup

--- CLBusinessLayer/ManageBL.cs
+++ CLBusinessLayer/ManageBL.cs
@@ -5,104 +5,72 @@ namespace CLBusinessLayer
 {
     public class ManageBL
     {
-        //private readonly PrimeVisionContext _context;
-        //private ManageDL oDL = null;
-
-        //public ManageBL(PrimeVisionContext context)
-        //{
-        //    _context = context;
-
-        //}
-
-        //public async Task<List<TFilm>> GetAllFilms()
-        //{
-        //    List<TFilm> listFilm = null;
-
-        //     oDL = new ManageDL(_context);
-        //     listFilm = await oDL.GetAllFilms();
-
-
-        //    return listFilm;
-        //}
-
-        //public async Task<TFilm> GetDetailsFilm(int id)
-        //{
-        //    TFilm oFilm = null;
-
-
-        //      oDL = new ManageDL(_context);
-        //      oFilm = await oDL.GetDetailsFilm(id);
-
-
-
-        //    return oFilm;
-        //}
-
-        //public async Task<int> AddFilm(TFilm oFilm)
-        //{
-        //    int iRet = 0;
-
-        //    oDL = new ManageDL(_context);
-        //    iRet = await oDL.AddFilm(oFilm);
-
-        //    return iRet;
-        //}   
-
-        //public async Task<int> UpdateFilm(TFilm oFilm)
-        //{
-        //    int iRet = 0;
-
-        //    oDL = new ManageDL(_context);
-        //    iRet = await oDL.UpdateFilm(oFilm);
-
-        //    return iRet;
-        //}
-
-        //public async Task<int> DeleteFilm(int id)
-        //{
-        //    int iRet = 0;
-
-        //    oDL = new ManageDL(_context);
-        //    iRet = await oDL.DeleteFilm(id);
-
-
-        //    return iRet;
-        //}
-
-
         private readonly PrimeVisionContext _context;
-        private readonly ManageDL _oDL;
+        private ManageDL oDL = null;
 
         public ManageBL(PrimeVisionContext context)
         {
             _context = context;
-            _oDL = new ManageDL(_context);
+
         }
 
-        public async Task<List<T>> GetAll<T>() where T : class
+        public async Task<List<TFilm>> GetAllFilms()
         {
-            return await _oDL.GetAll<T>();
+            List<TFilm> listFilm = null;
+
+            oDL = new ManageDL(_context);
+            listFilm = await oDL.GetAllFilms();
+
+
+            return listFilm;
         }
 
-        public async Task<T> GetDetails<T>(int id) where T : class
+        public async Task<TFilm> GetDetailsFilm(int id)
         {
-            return await _oDL.GetDetails<T>(id);
+            TFilm oFilm = null;
+
+
+            oDL = new ManageDL(_context);
+            oFilm = await oDL.GetDetailsFilm(id);
+
+
+
+            return oFilm;
         }
 
-        public async Task<int> Add<T>(T entity) where T : class
+        public async Task<int> AddFilm(TFilm oFilm)
         {
-            return await _oDL.Add(entity);
+            int iRet = 0;
+
+            oDL = new ManageDL(_context);
+            iRet = await oDL.AddFilm(oFilm);
+
+            return iRet;
         }
 
-        public async Task<int> Update<T>(T entity) where T : class
+        public async Task<int> UpdateFilm(TFilm oFilm)
         {
-            return await _oDL.Update(entity);
+            int iRet = 0;
+
+            oDL = new ManageDL(_context);
+            iRet = await oDL.UpdateFilm(oFilm);
+
+            return iRet;
         }
 
-        public async Task<int> Delete<T>(int id) where T : class
+        public async Task<int> DeleteFilm(int id)
         {
-            return await _oDL.Delete<T>(id);
+            int iRet = 0;
+
+            oDL = new ManageDL(_context);
+            iRet = await oDL.DeleteFilm(id);
+
+
+            return iRet;
         }
+
+
+
 
     }
 }
--- CLDataLayer/ManageDL.cs
+++ CLDataLayer/ManageDL.cs
@@ -5,71 +5,6 @@ namespace CLDataLayer
 {
     public class ManageDL
     {
-        //private readonly PrimeVisionContext _context;
-
-        //public ManageDL(PrimeVisionContext context)
-        //{
-        //    _context = context;
-        //}
-
-        //public async Task<List<TFilm>> GetAllFilms()
-        //{
-        //    List<TFilm> listFilm = null;
-
-        //    listFilm = await _context.TFilms.ToListAsync();
-
-
-        //    return listFilm;
-        //}
-
-        //public async Task<TFilm> GetDetailsFilm(int id)
-        //{
-        //    TFilm oFilm = null;
-
-        //    oFilm = await _context.TFilms.FindAsync(id);
-
-
-        //    return oFilm;
-        //}
-
-        //public async Task<int> AddFilm(TFilm oFilm)
-        //{
-        //    int iRet = 0;
-
-        //    _context.TFilms.Add(oFilm);
-        //    iRet = await _context.SaveChangesAsync();
-
-
-        //    return iRet;
-        //}
-
-        //public async Task<int> UpdateFilm(TFilm oFilm)
-        //{
-        //    int iRet = 0;
-
-        //    _context.Entry(oFilm).State = EntityState.Modified;
-        //    iRet = await _context.SaveChangesAsync();
-
-
-        //    return iRet;
-        //}
-
-        //public async Task<int> DeleteFilm(int id)
-        //{
-        //    int iRet = 0;
-
-        //    var oFilm = await _context.TFilms.FindAsync(id);
-        //    if (oFilm == null)
-        //    {
-        //        return iRet;
-        //    }
-
-        //    _context.TFilms.Remove(oFilm);
-        //    iRet = await _context.SaveChangesAsync();
-
-        //    return iRet;
-        //}
-
         private readonly PrimeVisionContext _context;
 
         public ManageDL(PrimeVisionContext context)
@@ -77,38 +12,65 @@ namespace CLDataLayer
             _context = context;
         }
 
-        public async Task<List<T>> GetAll<T>() where T : class
+        public async Task<List<TFilm>> GetAllFilms()
         {
-            return await _context.Set<T>().ToListAsync();
+            List<TFilm> listFilm = null;
+
+            listFilm = await _context.TFilms.ToListAsync();
+
+
+            return listFilm;
         }
 
-        public async Task<T> GetDetails<T>(int id) where T : class
+        public async Task<TFilm> GetDetailsFilm(int id)
         {
-            return await _context.Set<T>().FindAsync(id);
+            TFilm oFilm = null;
+
+            oFilm = await _context.TFilms.FindAsync(id);
+
+
+            return oFilm;
         }
 
-        public async Task<int> Add<T>(T entity) where T : class
+        public async Task<int> AddFilm(TFilm oFilm)
         {
-            _context.Set<T>().Add(entity);
-            return await _context.SaveChangesAsync();
+            int iRet = 0;
+
+            _context.TFilms.Add(oFilm);
+            iRet = await _context.SaveChangesAsync();
+
+
+            return iRet;
         }
 
-        public async Task<int> Update<T>(T entity) where T : class
+        public async Task<int> UpdateFilm(TFilm oFilm)
         {
-            _context.Entry(entity).State = EntityState.Modified;
-            return await _context.SaveChangesAsync();
+            int iRet = 0;
+
+            _context.Entry(oFilm).State = EntityState.Modified;
+            iRet = await _context.SaveChangesAsync();
+
+
+            return iRet;
         }
 
-        public async Task<int> Delete<T>(int id) where T : class
+        public async Task<int> DeleteFilm(int id)
         {
-            var entity = await _context.Set<T>().FindAsync(id);
-            if (entity == null)
+            int iRet = 0;
+
+            var oFilm = await _context.TFilms.FindAsync(id);
+            if (oFilm == null)
             {
-                throw new ArgumentException($"Entity of type {typeof(T).Name} with ID {id} non trovata.");
+                return iRet;
             }
-            _context.Set<T>().Remove(entity);
-            return await _context.SaveChangesAsync();
+
+            _context.TFilms.Remove(oFilm);
+            iRet = await _context.SaveChangesAsync();
+
+            return iRet;
         }
+
+
 
     }
 }
